20dea140126b83beca636e560df4932d92e278d9
itzbenz <49940811+Itzbenz@users.noreply.github.com>
2021-05-16 09:57:50 +0200

    h

--- src/main/java/Premain/Main.java
+++ src/main/java/Premain/Main.java
@@ -1,35 +1,14 @@
 package Premain;
 
-import arc.Core;
-import arc.Events;
-import arc.scene.ui.Dialog;
-import mindustry.Vars;
-import mindustry.game.EventType;
-import mindustry.gen.Icon;
 import mindustry.mod.Mod;
 
 public class Main extends Mod {
-	boolean stat;
+	static {//Classloader.loadClass moment
+		//Vars.platform.loadJar()
+	}
+	
 	@Override
 	public void init() {
-		//do you like lambda ?
-		Core.settings.getBoolOnce("Test", ()->{
-			Events.on(EventType.ClientLoadEvent.class, s ->{
-				new Dialog("Test"){{
-					if (Vars.mobile) buttons.button("Close", this::hide).growX();//mobile friendly on portrait
-					else buttons.button("Close", this::hide).size(210f, 64f);//what with this magic number
-					//garbage here
-					cont.button("Yeet", Icon.image, ()->{
-						Vars.ui.showInfo("Yeet");
-					}).growX();//stretch
-				}}.show();
-			});
-		});//sike how to color bracket
-		Vars.ui.settings.main.table(t ->{
-			t.check("Boolean [red][Test]", s->{
-				stat = s;//assign to global ?
-				Vars.ui.showInfo(stat+"");//toString()
-			});
-		}).growX();//stretch
+	
 	}
 }
