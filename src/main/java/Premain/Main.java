994638f0d0023ad94b67c2d1797af961ac62970d
itzbenz <49940811+Itzbenz@users.noreply.github.com>
2021-05-16 10:07:04 +0200

    aa

--- src/main/java/Premain/Main.java
+++ src/main/java/Premain/Main.java
@@ -3,12 +3,12 @@ package Premain;
 import mindustry.mod.Mod;
 
 public class Main extends Mod {
-	static {//Classloader.loadClass moment
-		//Vars.platform.loadJar()
-	}
-	
-	@Override
-	public void init() {
-	
-	}
+    static {//Classloader.loadClass moment
+        //Vars.platform.loadJar()
+    }
+    
+    @Override
+    public void init() {
+    
+    }
 }
