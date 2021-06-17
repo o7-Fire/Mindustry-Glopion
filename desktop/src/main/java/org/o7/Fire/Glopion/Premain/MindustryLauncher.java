0909aeab3b36f1ba3f62eb94568abf02f7ac02b2
itzbenz <49940811+Itzbenz@users.noreply.github.com>
2021-06-17 03:54:24 +0200

    save me a bug

--- desktop/src/main/java/org/o7/Fire/Glopion/Premain/MindustryLauncher.java
+++ desktop/src/main/java/org/o7/Fire/Glopion/Premain/MindustryLauncher.java
@@ -1,12 +1,14 @@
 package org.o7.Fire.Glopion.Premain;
 
 import Atom.Reflect.Reflect;
+import arc.Events;
 import arc.files.Fi;
 import arc.util.Log;
 import mindustry.ClientLauncher;
 import mindustry.Vars;
 import mindustry.core.Platform;
 import mindustry.desktop.DesktopLauncher;
+import mindustry.game.EventType;
 import mindustry.mod.ModClassLoader;
 import net.jpountz.util.Native;
 
@@ -22,20 +24,38 @@ public class MindustryLauncher {
         if(Reflect.debug){
             System.out.println(MindustryLauncher.class.getClassLoader().getClass().getCanonicalName());
             System.out.println(ModClassLoader.class.getClassLoader().getClass().getCanonicalName());
+            registerPatcher();
         }
         try {
             Native.load();
         }catch(Throwable t){
-            System.out.println(t);
+            Log.warn("Failed to load LZ4Factory Native online multiplayer performance may be degraded: @",t);
         }
         DesktopLauncher.main(args);
     }
-    
-    /**  patch after Vars.mods {@link ClientLauncher#setup()} to gain classloader control over mods
+    public static void registerPatcher(){
+        //Use ClientCreateEvent as Logger is not initialized
+        Events.on(EventType.ClientCreateEvent.class,s->{
+            //at this point Vars.platform != this
+            //so we need to wait a few more line
+            Log.LogHandler original = Log.logger;
+            Log.logger = (e, a) ->{
+                if(a.startsWith("[GL] Version:")){
+                    //Vars.platform == this
+                    //patch time
+                    patchClassloader();
+                    //return to original to avoid conflict
+                    Log.logger = original;
+                }
+                original.log(e,a);
+            };
+        });
+    }
+    /**  patch after Vars.platform = this {@link ClientLauncher#setup()} to gain classloader control over mods
      * i use Intellij breakpoint evaluate
      * */
     public static void patchClassloader(){
-        Platform inert = new Platform() {};
+        Platform inert = Vars.platform;
         Vars.platform = new Platform() {
             @Override
             public ClassLoader loadJar(Fi jar, ClassLoader parent) throws Exception {
