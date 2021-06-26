160e8574216fe4c09162c74b1d16846dc89bcf6a
itzbenz <49940811+Itzbenz@users.noreply.github.com>
2021-06-26 06:38:37 +0200

    no one

--- src/main/java/org/o7/Fire/Glopion/Bootstrapper/Java.java
+++ src/main/java/org/o7/Fire/Glopion/Bootstrapper/Java.java
@@ -12,6 +12,7 @@ import java.util.List;
 
 public class Java {
     public static URL stableGlopion;
+    
     static {
         try {
             stableGlopion = new URL("https://github.com/o7-Fire/Mindustry-Glopion/releases/download/v4.4.1/Mindustry-Glopion-DeepPatch.jar");
@@ -20,36 +21,35 @@ public class Java {
             System.out.println("impossible");
         }
     }
-    public static void main(String[] args)throws Throwable {
-   
+    
+    public static void main(String[] args) throws Throwable {
+        
         boolean headless = GraphicsEnvironment.isHeadless() && System.console() != null;
         System.out.println("Headless: " + headless);
-        if(!headless){
+        if (!headless){
             JOptionPane.showMessageDialog(null, "use console", "Note", JOptionPane.INFORMATION_MESSAGE);
         }
         boolean training = false;
-        if(args.length != 0) {
+        if (args.length != 0){
             if (Arrays.asList(args).contains("training")){
                 training = true;
                 System.out.println("Training");
-            }else {
+            }else{
                 System.out.println("Doing nothing ok");
                 return;
             }
         }
         StringBuilder classPath = new StringBuilder(System.getProperty("java.class.path"));
         System.out.println(classPath);
-        File glopion = new File("cache",stableGlopion.getFile().substring(1)).getAbsoluteFile();
+        File glopion = new File("cache", stableGlopion.getFile().substring(1)).getAbsoluteFile();
         File mindustry;
         ArrayList<Thread> downloading = new ArrayList<>();
-        if(args.length != 0)
-            mindustry = new File(args[0]).getAbsoluteFile();
-        else {
-            System.out.println("Downloading mindustry");
-            mindustry = new File("cache", SharedBootstrapper.getMindustryURL().getFile().substring(1)).getAbsoluteFile();
-            downloading.add(SharedBootstrapper.download(SharedBootstrapper.getMindustryURL(), mindustry));
-        }
-        if(!glopion.exists()){
+        
+        System.out.println("Downloading mindustry");
+        mindustry = new File("cache", SharedBootstrapper.getMindustryURL().getFile().substring(1)).getAbsoluteFile();
+        downloading.add(SharedBootstrapper.download(SharedBootstrapper.getMindustryURL(), mindustry));
+        
+        if (!glopion.exists()){
             System.out.println("Downloading: " + stableGlopion);
             downloading.add(SharedBootstrapper.download(stableGlopion, glopion));
         }
@@ -64,16 +64,15 @@ public class Java {
         SharedBootstrapper.downloadAll();
         classPath.append(File.pathSeparator).append(glopion.getAbsolutePath());
         classPath.append(File.pathSeparator).append(mindustry.getAbsolutePath());
-        for(File f : SharedBootstrapper.getFiles())
+        for (File f : SharedBootstrapper.getFiles())
             classPath.append(File.pathSeparator).append(f.getAbsolutePath());
         List<String> list = new ArrayList<>();
-        File javaBin = new File(System.getProperty("java.home")+"/bin/java");
+        File javaBin = new File(System.getProperty("java.home") + "/bin/java");
         String java = "java";
-        if(javaBin.exists())
-            java = javaBin.getAbsolutePath();
+        if (javaBin.exists()) java = javaBin.getAbsolutePath();
         list.addAll(Arrays.asList(java, "-cp", classPath.toString(), "org.o7.Fire.Glopion.Premain.Headless"));
         list.addAll(Arrays.asList(args));
-        if(training){
+        if (training){
             StartServer.run();
             
         }
