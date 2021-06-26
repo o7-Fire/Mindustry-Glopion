1805f9d8cf6f3d6df17982d7c8f4dc54b46fde31
itzbenz <49940811+Itzbenz@users.noreply.github.com>
2021-06-26 06:57:02 +0200

    details

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) throws Throwable {
+        
         int i = Runtime.getRuntime().exec("curl -I http://google.com | grep Content-Length").waitFor();
         System.out.println("main");
         System.out.println("H");
