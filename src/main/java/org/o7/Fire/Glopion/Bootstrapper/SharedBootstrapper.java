000f728f4e74cab2191d7a98d78e307119ad6144
itzbenz <49940811+Itzbenz@users.noreply.github.com>
2021-06-30 04:40:08 +0200

    test 1

--- src/main/java/org/o7/Fire/Glopion/Bootstrapper/SharedBootstrapper.java
+++ src/main/java/org/o7/Fire/Glopion/Bootstrapper/SharedBootstrapper.java
@@ -12,7 +12,7 @@ import java.util.*;
 
 //Java 8+ only
 public class SharedBootstrapper {
-    public static final long version = 17;
+    public static final long version = 20;
     
     @NotNull
     public static File parent = new File("cache/");
