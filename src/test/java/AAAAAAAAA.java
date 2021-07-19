83af1ad285966bbe47030823cb1547346dd1699a
itzbenz <49940811+Itzbenz@users.noreply.github.com>
2021-07-19 00:41:19 -0500

    should be running test now

--- src/test/java/AAAAAAAAA.java
+++ src/test/java/AAAAAAAAA.java
@@ -4,6 +4,23 @@ import org.o7.Fire.Glopion.Bootstrapper.StartServer;
 
 public class AAAAAAAAA {
     @Test
+    void sanityTest() {
+        for (int i = 0; i < 100; i++) {
+            System.out.println("AAAAAAAA: " + i);
+        }
+    }
+    
+    @Test
+    void changeGlobal() {
+        System.setProperty("Yeeeet", "1");
+    }
+    
+    @Test
+    void checkGlobal() {
+        assert System.getProperty("Yeeeet") == null;
+    }
+    
+    @Test
     void bootstrapperJavaTest() throws Throwable {
         Java.test = true;
         Java.main(new String[]{"test"});
