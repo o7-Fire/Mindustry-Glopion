5210f757244eb75686021418e5030bc640705e65
itzbenz <49940811+Itzbenz@users.noreply.github.com>
2021-07-26 03:15:14 -0500

    128.1

--- core/src/main/java/org/o7/Fire/Glopion/Version.java
+++ core/src/main/java/org/o7/Fire/Glopion/Version.java
@@ -22,7 +22,7 @@ import java.nio.file.Files;
 import java.nio.file.StandardOpenOption;
 
 public class Version {
-    public static String desktop = "0.5.9";
+    public static String desktop = "0.6.0";
     public static String core = "0.6.6";
     
     public static boolean isCore() {
