e395ebe3b9698dccef587fb8e6c836897b2592c9
itzbenz <49940811+Itzbenz@users.noreply.github.com>
2021-08-08 12:05:22 +0200

    fffs

---
+++ desktop/src/main/java/org/o7/Fire/Glopion/Desktop/OpenCV/MindsutryOpenCV.java
@@ -0,0 +1,11 @@
+package org.o7.Fire.Glopion.Desktop.OpenCV;
+
+import org.o7.Fire.Glopion.Module.ModsModule;
+
+public class MindsutryOpenCV extends ModsModule {
+    @Override
+    public void start() {
+        super.start();
+        
+    }
+}
