75b881cb664ec1ba63150cc62cffb073f04470ec
Itzbenz <49940811+Itzbenz@users.noreply.github.com>
2021-08-26 09:11:43 +0200

    Update runTestAndroid.sh

--- runTestAndroid.sh
+++ runTestAndroid.sh
@@ -2,5 +2,6 @@ echo "h"
 wget https://github.com/o7-Fire/Static-Mindustry-Package/releases/download/v130.1/mindustry.apk
 adb shell ls /system/bin
 adb install mindustry.apk
-adb shell ls /storage/emulated/0/Android
-
+adb push ./build/libs/Mindustry-Glopion-Dexed.jar /storage/emulated/0/Android/data/io.anuken.mindustry/files/mods/Mindustry-Glopion-Dexed.jar
+adb shell ls /storage/emulated/0/Android/data/io.anuken.mindustry/files/mods
+adb shell am start -n io.anuke.mindustry/mindustry.android.AndroidLauncher
