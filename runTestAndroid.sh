f5ea77569317417a6d481112b7be922f9a3dfc58
Itzbenz <49940811+Itzbenz@users.noreply.github.com>
2021-08-26 08:49:54 +0200

    Update runTestAndroid.sh

--- runTestAndroid.sh
+++ runTestAndroid.sh
@@ -1,7 +1,6 @@
 echo "h"
 wget https://github.com/o7-Fire/Static-Mindustry-Package/releases/download/v130.1/mindustry.apk
 adb shell ls /system/bin
-adb mindustry.apk
-adb shell tree
-adb shell ls /storage/emulated/0/Android/data
+adb install mindustry.apk
+adb shell ls /storage/emulated/0/Android
 
